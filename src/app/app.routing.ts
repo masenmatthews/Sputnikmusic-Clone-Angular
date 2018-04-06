f96a0b64678c58006206094d44b9d21526ee01d5
Masen Matthews <masenmatthews@gmail.com>
2018-04-06 09:28:54 -0700

    add routing to reviews page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'reviews',
+    component: ReviewsComponent
   }
 ];
 
