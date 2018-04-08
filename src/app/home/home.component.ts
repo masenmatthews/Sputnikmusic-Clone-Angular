8c8735b9d62c7aa8ee6be81ebb6a38a0bb42cad2
Masen Matthews <masenmatthews@gmail.com>
2018-04-08 16:35:50 -0700

    troubleshooting continued

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,7 +23,7 @@ export class HomeComponent implements OnInit {
   }
 
   goToDetailPage(clickedReview) {
-    this.router.navigate(['reviews/:id', clickedReview.$key]);
+    this.router.navigate(['reviews', clickedReview.$key]);
   };
 
 }
